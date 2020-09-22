c2afb984892c1446c31a140be9701d8eee037c3d
Heesang whang <hwhang0917@gmail.com>
2020-09-22 10:35:14 +0900

    🎨 Update GraphQL schema directory to be relative

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -27,6 +27,7 @@ const resolvers = {
       }: { showId: number; seasonNumber: number; episodeNumber: number }
     ) => show.episodeDetail(showId, seasonNumber, episodeNumber),
     searchShow: (_: null, { term }: { term: string }) => show.search(term),
+
     //   Person Resolvers
     personDetail: (_: null, { id }: { id: number }) => people.personDetail(id),
     searchPerson: (_: null, { term }: { term: string }) => people.search(term),
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
+import * as path from "path";
+
+const schema: string = path.resolve(__dirname, "graphql", "schema.graphql");
 
 const server = new GraphQLServer({
-  typeDefs: "src/graphql/schema.graphql",
+  typeDefs: schema,
   resolvers,
 });
 
