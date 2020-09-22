365f09418d947b3a6e08800356bce38d879b7157
Heesang whang <hwhang0917@gmail.com>
2020-09-23 03:31:37 +0900

    🔨 Add netlify lambda build scripts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, GraphQLServerLambda } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
 import * as path from "path";
 
 const schema: string = path.resolve(__dirname, "graphql", "schema.graphql");
 
-const server = new GraphQLServer({
-  typeDefs: schema,
-  resolvers,
-});
+if (process.env.NODE_ENV === "production") {
+  // Serverless function for production
+  const lambda = new GraphQLServerLambda({ typeDefs: schema, resolvers });
+  exports.handler = lambda.handler;
+} else if (process.env.NODE_ENV === "development") {
+  // GraphQL playground server for development
+  const server = new GraphQLServer({
+    typeDefs: schema,
+    resolvers,
+  });
 
-server.start(() =>
-  console.log(`✅ GraphQL server is running on http://localhost:4000`)
-);
+  const options = { port: 4000 };
+
+  server.start(() =>
+    console.log(
+      options,
+      `✅ GraphQL server is running on http://localhost:${options.port}`
+    )
+  );
+}
