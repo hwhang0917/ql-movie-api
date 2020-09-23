1ab67137644ba6002209f2f664ccbc0964bdbe76
Heesang whang <hwhang0917@gmail.com>
2020-09-24 02:13:09 +0900

    🔥 Remove unused string schema

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { GraphQLServer, GraphQLServerLambda } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
 import * as path from "path";
-// import graphQLStringSchema from "./schema";
 
 const schema: string = path.resolve(__dirname, "graphql", "schema.graphql");
 
 if (process.env.NODE_ENV === "production") {
   // Serverless function for production
   const lambda = new GraphQLServerLambda({
-    typeDefs: "./graphql/schema.graphql",
+    typeDefs: schema,
     resolvers,
   });
   exports.handler = lambda.handler;
