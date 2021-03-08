8b1bcc3f90187e6b8154f6e7761c16bdeb8dbe8d
Heesang whang <hwhang0917@gmail.com>
2021-03-08 18:40:08 +0900

    :wrench: Update app module cofig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,9 +6,32 @@ import { MoviesModule } from './movies/movies.module';
 import { ShowsModule } from './shows/shows.module';
 import { PeopleModule } from './people/people.module';
 import { CommonModule } from './common/common.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [MoviesModule, ShowsModule, PeopleModule, CommonModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        TMDB_API: Joi.string().required(),
+        TMDB_API_KEY: Joi.string().required(),
+      }),
+    }),
+    GraphQLModule.forRoot({ autoSchemaFile: true }),
+    ApiModule.forRoot({
+      apiDomain: process.env.TMDB_API,
+      apiKey: process.env.TMDB_API_KEY,
+      language: 'ko-KR',
+      region: 'KR',
+    }),
+    MoviesModule,
+    ShowsModule,
+    PeopleModule,
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
