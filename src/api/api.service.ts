e60b5c8d59a9eb188db6affb669b5ce44c9ccaa9
Heesang Whang <hwhang0917@gmail.com>
2021-04-05 19:41:59 +0900

    :fire: Remove unnecessary async/await

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -44,65 +44,60 @@ export class ApiService {
   movies = {
     nowPlaying: (): Promise<Movie[]> => this.get<Movie[]>('movie/now_playing'),
 
-    upcoming: async (): Promise<Movie[]> => this.get<Movie[]>('movie/upcoming'),
+    upcoming: (): Promise<Movie[]> => this.get<Movie[]>('movie/upcoming'),
 
-    popular: async (): Promise<Movie[]> =>
-      await this.get<Movie[]>('movie/popular'),
+    popular: (): Promise<Movie[]> => this.get<Movie[]>('movie/popular'),
 
-    findById: async (id: number): Promise<Movie> =>
-      await this.get<Movie>(`movie/${id}`, {
+    findById: (id: number): Promise<Movie> =>
+      this.get<Movie>(`movie/${id}`, {
         append_to_response: 'videos,credits',
       }),
 
-    findSimilarById: async (id: number): Promise<Movie[]> =>
-      await this.get<Movie[]>(`movie/${id}/similar`),
+    findSimilarById: (id: number): Promise<Movie[]> =>
+      this.get<Movie[]>(`movie/${id}/similar`),
   };
 
   shows = {
-    topRated: async (): Promise<Show[]> => this.get<Show[]>('tv/top_rated'),
+    topRated: (): Promise<Show[]> => this.get<Show[]>('tv/top_rated'),
 
-    popular: async (): Promise<Show[]> => this.get<Show[]>('tv/popular'),
+    popular: (): Promise<Show[]> => this.get<Show[]>('tv/popular'),
 
-    airingToday: async (): Promise<Show[]> =>
-      this.get<Show[]>('tv/airing_today'),
+    airingToday: (): Promise<Show[]> => this.get<Show[]>('tv/airing_today'),
 
-    findById: async (id: number): Promise<Show> =>
+    findById: (id: number): Promise<Show> =>
       this.get<Show>(`tv/${id}`, {
         append_to_response: 'videos,credits',
       }),
 
-    findSimilarById: async (id: number): Promise<Show[]> =>
-      await this.get<Show[]>(`tv/${id}/similar`),
+    findSimilarById: (id: number): Promise<Show[]> =>
+      this.get<Show[]>(`tv/${id}/similar`),
 
-    getSeasonDetail: async (
-      showId: number,
-      seasonNumber: number,
-    ): Promise<Season> =>
-      await this.get<Season>(`tv/${showId}/season/${seasonNumber}`),
+    getSeasonDetail: (showId: number, seasonNumber: number): Promise<Season> =>
+      this.get<Season>(`tv/${showId}/season/${seasonNumber}`),
 
-    getEpisodeDetail: async (
+    getEpisodeDetail: (
       showId: number,
       seasonNumber: number,
       episodeNumber: number,
     ): Promise<Episode> =>
-      await this.get<Episode>(
+      this.get<Episode>(
         `tv/${showId}/season/${seasonNumber}/episode/${episodeNumber}`,
       ),
   };
 
   people = {
-    findPersonById: async (id: number): Promise<Person> =>
-      await this.get<Person>(`person/${id}`),
+    findPersonById: (id: number): Promise<Person> =>
+      this.get<Person>(`person/${id}`),
   };
 
   search = {
-    movie: async (query: string): Promise<Movie[]> =>
-      await this.get<Movie[]>('search/movie', { query }),
+    movie: (query: string): Promise<Movie[]> =>
+      this.get<Movie[]>('search/movie', { query }),
 
-    show: async (query: string): Promise<Show[]> =>
-      await this.get<Show[]>('search/tv', { query }),
+    show: (query: string): Promise<Show[]> =>
+      this.get<Show[]>('search/tv', { query }),
 
-    person: async (query: string): Promise<Person[]> =>
-      await this.get<Person[]>('search/person', { query }),
+    person: (query: string): Promise<Person[]> =>
+      this.get<Person[]>('search/person', { query }),
   };
 }
