586b43c33bb40f7016152eca3ad1fb0d98430d05
Heesang Whang <hwhang0917@gmail.com>
2021-04-06 17:48:46 +0900

    :art: Seprate error messages

---
+++ src/errors/errors.ts
@@ -0,0 +1,7 @@
+export const errorMessage = {
+  showNotFound: 'Show with that id does not exists.',
+  seasonNotFound: 'Season number does not exists.',
+  episodeNotFound: 'Episode number does not exists.',
+  movieNotFound: 'Movie with that id does not exists.',
+  personNotFound: 'Person with that id does not exists.',
+};
